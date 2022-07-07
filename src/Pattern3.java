47f41221da37d12904eee53e0847c3313a89028c
PuniKisan <punitcode18@gmail.com>
2022-07-07 23:34:39 +0530

    Added Pattern 3

---
+++ src/Pattern3.java
@@ -0,0 +1,18 @@
+/*
+3. Print the pattern
+    *
+    **
+    ***
+    ****
+*/
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
